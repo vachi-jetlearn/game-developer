d3518b825d8e41609d014865e9f684a3b4d46ca6
vachi-jetlearn <vachigupta08@gmail.com>
2026-07-27 18:54:24 +0800

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,25 +1,57 @@
-import pgzrun,os
+import pgzrun,os,time
 os.environ["SDL_VIDEO_CENTERED"]="1"
 
 WIDTH=1000
 HEIGHT=800
 
+#variables
+
+time1=15
+
+list=[]
 
 
-answer1=Rect(50,350,425,200)
-answer2=Rect(550,350,425,200)
-answer3=Rect(50,575,425,200)
-answer4=Rect(550,575,425,200)
-question=Rect(50,25,700,100)
+#Rectangles
+answer1=Rect(50,200,425,200)
+answer2=Rect(550,200,425,200)
+answer3=Rect(50,425,425,200)
+answer4=Rect(550,425,425,200)
+question=Rect(50,75,700,100)
+timer=Rect(770,75,180,100)
+moving=Rect(50,10,900,55)
+skip=Rect(50,650,925,100)
+
 
 def draw():
     screen.fill((100,100,100))
-    screen.draw.filled_rect(answer1,(244,45,54))
-    screen.draw.filled_rect(answer2,(144,98,165))
-    screen.draw.filled_rect(answer3,(10,201,165))
-    screen.draw.filled_rect(answer4,(234,43,165))
+    screen.draw.filled_rect(answer1,"red")
+    screen.draw.filled_rect(answer2,"blue")
+    screen.draw.filled_rect(answer3,"yellow")
+    screen.draw.filled_rect(answer4,"green")
     screen.draw.filled_rect(question,(255,60,255))
+    screen.draw.filled_rect(timer,(5,60,255))
+    screen.draw.filled_rect(moving,(5,0,5))
+    screen.draw.filled_rect(skip,(255,255,255))
 
+    screen.draw.textbox(("Quizmaster"),moving,color="white")
+    screen.draw.textbox(("SKIP"),skip, color="black",shadow=(0.5,0.2), scolor="pink")
+    screen.draw.textbox((str(time1)),timer, color="white")
+
+def update():
+    global time1
+    moving.x+=4
+    if moving.x>1000:
+        moving.right=0
+
+
+def timee():
+    global time1
+    if time1>0:
+     time1=time1-1
+    print(time1)
+
+
+clock.schedule_interval(timee,1)
 
 
 
