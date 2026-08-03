dd13aa992474df6a90a22bc64dfedddd8f2275eb
vachi-jetlearn <vachigupta08@gmail.com>
2026-08-03 18:59:46 +0800

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -1,15 +1,48 @@
-import pgzrun,os,time
+import pgzrun,os,time,random
 os.environ["SDL_VIDEO_CENTERED"]="1"
 
 WIDTH=1000
 HEIGHT=800
+
+game_over=False
 
 #variables
 
 time1=15
 
 list=[]
+path=("/Users/vachigupta/coding/python game developer/questions.txt")
 
+def readq():
+   global list
+   file=open(path,"r")
+   for i in file:
+      list.append(i)
+   file.close()
+   print(list)
+
+def seperate():
+   global questans, game_over
+   random.shuffle(list)
+   question=list.pop(0)
+   questans=question.split(",")
+   print(questans)
+   if len(list)==0:
+      game_over=True
+      
+      
+   
+
+readq()
+
+
+
+
+
+questans=[]
+
+seperate()
+   
 
 #Rectangles
 answer1=Rect(50,200,425,200)
@@ -23,6 +56,8 @@ skip=Rect(50,650,925,100)
 
 
 def draw():
+    
+    
     screen.fill((100,100,100))
     screen.draw.filled_rect(answer1,"red")
     screen.draw.filled_rect(answer2,"blue")
@@ -36,6 +71,11 @@ def draw():
     screen.draw.textbox(("Quizmaster"),moving,color="white")
     screen.draw.textbox(("SKIP"),skip, color="black",shadow=(0.5,0.2), scolor="pink")
     screen.draw.textbox((str(time1)),timer, color="white")
+    screen.draw.textbox((questans[0]),question,color="white",)
+    screen.draw.textbox((questans[1]),answer1,color="white",)
+    screen.draw.textbox((questans[2]),answer2,color="white",)
+    screen.draw.textbox((questans[3]),answer3,color="white",)
+    screen.draw.textbox((questans[4]),answer4,color="white",)
 
 def update():
     global time1
@@ -50,6 +90,14 @@ def timee():
      time1=time1-1
     print(time1)
 
+def on_mouse_down(pos):
+   global questans
+   if skip.collidepoint(pos):
+      seperate()
+      if game_over==True:
+         questans=["Game Over!","-","-","-","-"]
+         
+      
 
 clock.schedule_interval(timee,1)
 
